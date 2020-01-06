25d8c6ed654dbeb55391245418d49fb41609c8f3
Jiwon <winning1993@naver.com>
2020-01-07 02:55:11 +0900

    ngrok-setting

--- init.js
+++ init.js
@@ -10,8 +10,20 @@ import "./model/embeddedVideo";
 import "./model/user";
 
 const PORT = process.env.PORT;
+const ngrok = process.env.ENABLE_TUNNEL ? require('ngrok') : false;
+
 
 const handleListening = () => {
+    if(ngrok) {
+        ngrok.connect(process.env.PORT, (err, url) =>{
+            if(err){
+                return console.log(err)
+            }
+            console.log(`Server started. Tunnel running at url ${url}`);
+        })
+    }else{
     console.log(`Listening on : ${PORT}`);
+    }
 }
+
 app.listen(PORT, handleListening);
