2e9885f4df318c34bf82adac68a0e17228fe8d07
Jiwon <winning1993@naver.com>
2020-01-11 03:12:05 +0900

    video comment submit without refresh (pending)

--- views/vanilla/videoDetail.js
+++ views/vanilla/videoDetail.js
@@ -1,10 +1,18 @@
+
+
 const shareSelector = document.getElementById("shareSelector");
 const sharedStatus = document.getElementById("sharedStatus");
+const postComment = document.getElementById("postComment");
+const contents = document.getElementById("contentsOfComment");
+const newComment = document.getElementById("newComment");
+const commentAuthor = document.getElementById("commentAuthor");
+const commentContents = document.getElementById("commentContents");
+const commentCreatedAt = document.getElementById("commentCreatedAt");
+   
 
-
+const videoId = window.location.href.split("/videos/")[1];
 
 const changeShareStatus = () => {
-    const videoId = window.location.href.split("/videos/")[1]
     fetch(`/videos/${videoId}/share-selector`,{method:"POST"});
     if(sharedStatus.innerHTML==="shared"){
     sharedStatus.innerHTML = "not shared"
@@ -13,8 +21,31 @@ const changeShareStatus = () => {
     }
 }
 
+const handleCommentSubmit = async(e) => {
+    e.preventDefault();
+    const commentValue = await contents.value;
+    const span = document.createElement("span");
+    commentAuthor.innerHTML = "You : "
+    commentContents.innerHTML = commentValue;
+    commentCreatedAt.innerHTML = new Date();
+
+    handleAddedComment(commentValue)
+}
+
+const handleAddedComment = async(commentValue) => {
+    const response = await axios({
+        url:`/videos/${videoId}/video-comment`,
+        method: "post",
+        data: {
+            comment: commentValue
+        }
+    })
+    console.log(response);
+}
+
 const apisInit = () => {
-    shareSelector.addEventListener("click", changeShareStatus)
+    shareSelector.addEventListener("click", changeShareStatus);
+    postComment.addEventListener("submit", handleCommentSubmit);
 }
 
 apisInit();
